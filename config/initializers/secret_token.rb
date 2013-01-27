5dc41ba56270e971a91ee27556a43051807c5c15
Geding <geding@o2.pl>
2013-01-27 12:05:39 +0100

    upload
    adding project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myblog::Application.config.secret_token = '0760703a0822f707f8ff905da73c7f02c09749aef4a4cb497fb6af386ad03d909b3e2df59e48f3758dbfeb316bd54894b8e0adfe22ccf177776ff5dae89226c3'
